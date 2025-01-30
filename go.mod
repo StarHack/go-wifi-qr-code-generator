7b9b8145abdea81f4e5318e3688255bf0c9e7a54
StarHack <danielschmidth@googlemail.com>
2025-01-30 13:53:08 +0100

    add support for logos

--- go.mod
+++ go.mod
@@ -1,5 +1,14 @@
 module wifipass
 
-go 1.21.1
+go 1.23
 
-require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+)
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.23.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"image/color"
+	"image/png"
 	"os"
 	"strings"
 
+	"github.com/fogleman/gg"
+	"github.com/nfnt/resize"
 	"github.com/skip2/go-qrcode"
 )
 
@@ -41,6 +45,82 @@ func readUserInput(instructions string, validTypes []string) (string, error) {
 	return userInput, nil
 }
 
+func addLogoToQR(qrImagePath, logoPath, outputPath string) error {
+	qrFile, err := os.Open(qrImagePath)
+	if err != nil {
+		return err
+	}
+	defer qrFile.Close()
+
+	qrImage, err := png.Decode(qrFile)
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(logoPath); os.IsNotExist(err) {
+		err := os.Rename(qrImagePath, outputPath)
+		if err != nil {
+			return err
+		}
+		fmt.Println("No logo found. QR code generated without branding.")
+		return nil
+	}
+
+	logoFile, err := os.Open(logoPath)
+	if err != nil {
+		return err
+	}
+	defer logoFile.Close()
+
+	logoImage, err := png.Decode(logoFile)
+	if err != nil {
+		return err
+	}
+
+	logoBounds := logoImage.Bounds()
+	logoWidth, logoHeight := logoBounds.Dx(), logoBounds.Dy()
+
+	const logoScale = 0.20
+	const marginScale = 1.4
+	qrSize := qrImage.Bounds().Dx()
+	maxLogoSize := float64(qrSize) * logoScale
+
+	var newLogoWidth, newLogoHeight uint
+	aspectRatio := float64(logoWidth) / float64(logoHeight)
+
+	if aspectRatio > 1 {
+		newLogoWidth = uint(maxLogoSize)
+		newLogoHeight = uint(maxLogoSize / aspectRatio)
+	} else {
+		newLogoHeight = uint(maxLogoSize)
+		newLogoWidth = uint(maxLogoSize * aspectRatio)
+	}
+
+	logoResized := resize.Resize(newLogoWidth, newLogoHeight, logoImage, resize.Lanczos3)
+
+	dc := gg.NewContext(qrSize, qrSize)
+	dc.DrawImage(qrImage, 0, 0)
+
+	circleRadius := maxLogoSize * marginScale
+	centerX, centerY := float64(qrSize)/2, float64(qrSize)/2
+
+	dc.SetColor(color.White)
+	dc.DrawCircle(centerX, centerY, circleRadius/2)
+	dc.Fill()
+
+	x := int(centerX - float64(newLogoWidth)/2)
+	y := int(centerY - float64(newLogoHeight)/2)
+	dc.DrawImage(logoResized, x, y)
+
+	outputFile, err := os.Create(outputPath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	return png.Encode(outputFile, dc.Image())
+}
+
 func main() {
 	ssid, err := readUserInput("Enter WiFi name (SSID): ", []string{})
 	if err != nil {
@@ -57,15 +137,24 @@ func main() {
 		panic(err)
 	}
 
-	// WIFI:T:<NetworkType>;S:<SSID>;P:<Password>;;
 	wifiString := fmt.Sprintf("WIFI:T:%s;S:%s;P:%s;;", networkType, ssid, password)
 
-	// QR encode and save to disk
-	err = qrcode.WriteFile(wifiString, qrcode.Medium, 256, "wifi-qr.png")
+	qrFilePath := "wifi-qr.png"
+	err = qrcode.WriteFile(wifiString, qrcode.High, 512, qrFilePath)
 	if err != nil {
 		fmt.Println("Failed to generate QR code:", err)
 		return
 	}
 
-	fmt.Println("QR code generated successfully: wifi-qr.png")
+	logoFilePath := "logo.png"
+	outputFilePath := "wifi-qr-branded.png"
+
+	err = addLogoToQR(qrFilePath, logoFilePath, outputFilePath)
+	if err != nil {
+		fmt.Println("Failed to add logo:", err)
+		return
+	}
+
+	fmt.Println("QR code generated successfully:", outputFilePath)
 }
+
